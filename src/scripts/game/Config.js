c1b218737fcc473778a597c7cf830940e65742e9
Do Do <dnkdo@tma.com.vn>
2023-11-29 09:30:02 +0700

    add gradient text

--- src/scripts/game/Config.js
+++ src/scripts/game/Config.js
@@ -2,8 +2,17 @@ import { Tools } from "../system/Tools";
 import { Game } from "./Game";
 
 export const Config = {
-    scenes: {
-        Game
-    },
-    loader: Tools.massiveRequire(require["context"]('./../../sprites/', true, /\.(mp3|png|jpe?g)$/))
+  scenes: {
+    Game,
+  },
+  loader: Tools.massiveRequire(
+    require["context"]("./../../sprites/", true, /\.(mp3|png|jpe?g)$/)
+  ),
+  grandText: {
+    text: "GRAND",
+    x: 100,
+    y: 100,
+    color: ["#ff9600", "#ff5200"],
+    fontSize: 50,
+  },
 };
--- src/scripts/system/App.js
+++ src/scripts/system/App.js
@@ -3,29 +3,29 @@ import { Loader } from "./Loader";
 import { ScenesManager } from "./ScenesManager";
 
 class Application {
-    run(config) {
-        this.config = config;
-        this.app = new PIXI.Application({resizeTo: window});
-        document.body.appendChild(this.app.view);
+  run(config) {
+    this.config = config;
+    this.app = new PIXI.Application({ resizeTo: window });
+    document.body.appendChild(this.app.view);
 
-        this.loader = new Loader(this.config);
-        this.loader.preload().then(() => this.start());
+    this.loader = new Loader(this.config);
+    this.loader.preload().then(() => this.start());
 
-        this.scenes = new ScenesManager();
-        this.app.stage.addChild(this.scenes.container);
-    }
+    this.scenes = new ScenesManager();
+    this.app.stage.addChild(this.scenes.container);
+  }
 
-    res(key) {
-        return this.loader.resources[key];
-    }
+  res(key) {
+    return this.loader.resources[key];
+  }
 
-    sprite(key) {
-        return new PIXI.Sprite(this.res(key));
-    }
+  sprite(key) {
+    return new PIXI.Sprite(this.res(key));
+  }
 
-    start() {
-        this.scenes.start("Game");
-    }
+  start() {
+    this.scenes.start("Game");
+  }
 }
 
 export const App = new Application();
